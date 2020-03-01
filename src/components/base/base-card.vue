b5354fb9ae4ff7380ae5195c8cb9b24a14ab8f74
Al-un <alun.sng@gmail.com>
2020-03-01 18:53:21 +0900

    Add an empty BaseCard component

---
+++ src/components/base/base-card.vue
@@ -0,0 +1,31 @@
+<template functional>
+  <div class="al-card">
+    <div class="al-card-header" v-if="slots()['header']">
+      <slot name="header"></slot>
+    </div>
+
+    <div class="al-card-body">
+      <slot></slot>
+    </div>
+
+    <div class="al-card-footer" v-if="slots()['footer']">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface Props {
+  expandable?: boolean;
+}
+
+export default Vue.extend<Props>({
+  name: "BaseInputSubmit",
+  functional: true,
+  props: {
+    expandable: { type: Boolean, required: false, default: undefined }
+  }
+});
+</script>
